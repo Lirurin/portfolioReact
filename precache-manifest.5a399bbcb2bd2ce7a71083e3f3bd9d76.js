90f72835199adeccb50ceac34a49e77c0fe5b434
Lirurin <zerosouler@gmail.com>
2020-03-04 14:10:43 +0300

    Updates

---
+++ precache-manifest.5a399bbcb2bd2ce7a71083e3f3bd9d76.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f578a6beac96677ddb5d50ee5292da03",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7432138ac9d80a69afed",
+    "url": "./static/css/main.cd787474.chunk.css"
+  },
+  {
+    "revision": "00acdee1e069d9dc6094",
+    "url": "./static/js/2.e1f8bd1a.chunk.js"
+  },
+  {
+    "revision": "681895645d25bb57ef2f39054354e4dd",
+    "url": "./static/js/2.e1f8bd1a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7432138ac9d80a69afed",
+    "url": "./static/js/main.b2971687.chunk.js"
+  },
+  {
+    "revision": "dcf4e72962e0e6fa882e",
+    "url": "./static/js/runtime-main.3d2bd151.js"
+  },
+  {
+    "revision": "4b44333bd02eef8ea281cf20af9eb1b4",
+    "url": "./static/media/Css.4b44333b.svg"
+  },
+  {
+    "revision": "81160c9903ebc64dfbef430cb52c65c2",
+    "url": "./static/media/Html.81160c99.svg"
+  },
+  {
+    "revision": "0a907085c7c975acd032246d69562514",
+    "url": "./static/media/React.0a907085.svg"
+  },
+  {
+    "revision": "d981344d5023aeb1a18e326b5f4369d9",
+    "url": "./static/media/Vue.d981344d.svg"
+  },
+  {
+    "revision": "f851967d69a5afed7d40a3b5df73bd8a",
+    "url": "./static/media/back1.f851967d.svg"
+  },
+  {
+    "revision": "333b507e8f4b3c035b25edbb6405af57",
+    "url": "./static/media/cloud-bg.333b507e.jpg"
+  },
+  {
+    "revision": "0560ad8e71290fe32417837db81e9567",
+    "url": "./static/media/exo-2.0560ad8e.woff"
+  },
+  {
+    "revision": "9bf53477ffff067cfe46289ae43790cb",
+    "url": "./static/media/exo-2.9bf53477.eot"
+  },
+  {
+    "revision": "a0776cbb819e23c3ec3b1d73f65443d4",
+    "url": "./static/media/exo-2.a0776cbb.svg"
+  },
+  {
+    "revision": "a5ff577049e0750e864af37722e204eb",
+    "url": "./static/media/exo-2.a5ff5770.woff2"
+  },
+  {
+    "revision": "ceb66eee436df1c3dd9e0f440add392f",
+    "url": "./static/media/exo-2.ceb66eee.ttf"
+  },
+  {
+    "revision": "2d8c8027895ea82d2a4ca2fcc6437b00",
+    "url": "./static/media/github-logo.2d8c8027.svg"
+  },
+  {
+    "revision": "f7342212b3c03a8c2e49afba4ab151d5",
+    "url": "./static/media/glitch.f7342212.jpg"
+  },
+  {
+    "revision": "58191b31cbe4cb96ba5761cf5684e133",
+    "url": "./static/media/javascript.58191b31.svg"
+  },
+  {
+    "revision": "f1269b0bf1d36606cee0a3e6e8f4d47a",
+    "url": "./static/media/mail.f1269b0b.svg"
+  },
+  {
+    "revision": "575de108914c21f945c15db0accecfcb",
+    "url": "./static/media/portfolio.575de108.svg"
+  },
+  {
+    "revision": "b3afd3db0bdf8be3553c3c287064d18e",
+    "url": "./static/media/taxi.b3afd3db.jpg"
+  },
+  {
+    "revision": "fe61049807d03baffb4982f72df9f766",
+    "url": "./static/media/user.fe610498.svg"
+  },
+  {
+    "revision": "da582e2b81ac62d76b3e0b7b26cdc9e3",
+    "url": "./static/media/voltonov.da582e2b.jpg"
+  }
+]);
